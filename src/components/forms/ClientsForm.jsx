7f3051d65d979ef9cef6a99e6ccb4ab9d53ee49c
vinn-as1999 <vinicyus.alves302@gmail.com>
2024-10-28 12:10:04 -0300

    new att

--- src/components/forms/ClientsForm.jsx
+++ src/components/forms/ClientsForm.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../../styles/Clients.css'
 
 function ClientsForm(props) {
-
+  const [name, setName] = useState('');
+  const [contact, setContact] = useState('');
+  const [clientAddress, setClientAddress] = useState('');
+  const [obs, setObs] = useState('');
 
   async function addClients() {
     const response = await fetch();
@@ -28,24 +31,24 @@ function ClientsForm(props) {
             <label>
                 Nome:
             </label>
-            <input type="text" />
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
 
             <label>
                 Contato:
             </label>
-            <input type="number" />
+            <input type="number" value={contact} onChange={e => setContact(e.target.value)} />
 
             <label>
                 Local/Endereço:
             </label>
-            <input type="text" />
+            <input type="text" value={clientAddress} onChange={e => setClientAddress(e.target.value)} />
 
             <label>
                 Observações:
             </label>
-            <textarea></textarea>
+            <textarea value={obs} onChange={e => setObs(e.target.value)} />
 
-            <button onClick={(e) => {addClients(); e.preventDefault()}}>
+            <button onClick={e => {addClients(); e.preventDefault()}}>
                 Salvar
             </button>
         </form>
