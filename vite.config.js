d38fdd98d9d0221a091a1b9e96cb87a64e4e8a0e
vinn-as1999 <vinicyus.alves302@gmail.com>
2024-11-27 11:22:32 -0300

    new update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://vinn-as1999.github.io/sales_controller/'
+  plugins: [react()]
 })
